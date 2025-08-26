4ff489893ffe6f2e347fd96b5ca42e2d9faaab33
Hüseyin Tayfun Kılıç <146750462+Hsenklc@users.noreply.github.com>
2025-08-27 02:03:30 +0300

    Add files via upload

---
+++ buton_gizleme/buton_gizleme/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace buton_gizleme
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Gösterme ve Gizleme Butonu
+        {
+            if (label1.Visible) //Yazı görülüyor mu diye kontrol ediyoruz
+            {
+                label1.Visible = false; // Eğer yazı görülüyorsa, onu gizliyoruz.
+                button1.Text = "Göster"; //Butonun üzerinde ki yazıyı "Göster" olarak değiştiriyoruz
+            }
+            else
+            {
+                label1.Visible = true; //Eğer yazı gizliyse, onu gösteriyoruz.
+                button1.Text = "Gizle"; //Butonun üzerinde ki yazıyı "Gizle" olarak değiştiriyoruz
+            }
+        }
+    }
+}
