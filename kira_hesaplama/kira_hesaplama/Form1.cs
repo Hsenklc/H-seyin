32813158ef55b4158f54555f1dcdfa330a4af9bf
Hüseyin Tayfun Kılıç <146750462+Hsenklc@users.noreply.github.com>
2025-08-27 02:07:24 +0300

    Add files via upload

---
+++ kira_hesaplama/kira_hesaplama/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kira_hesaplama
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Değişkenleri tanımlıyoruz
+            double kira;
+            double oran;
+            double yil;
+            double snc = 1;
+
+            kira = Convert.ToDouble(textBox1.Text); //Girilen kira değerini "kira" adlı değişkene atıyoruz.
+            oran = Convert.ToDouble(textBox2.Text); //Girilen oran değerini "oran" adlı değişkene atıyoruz.
+            yil = Convert.ToDouble(textBox3.Text); //Girilen yıl değerini "yıl" adlı değişkene atıyoruz.
+
+            for (int i = 0; i < yil; i++) //bir döngü oluşturuyoruz
+            {
+                snc = snc * (1 + oran / 100); //sonucu bulmak için matematiksel bir işlem yapıyoruz.
+            }
+
+            MessageBox.Show(snc.ToString()); //sonucu ekrana mesaj kutusu ile yazdırıyoruz.
+        }
+    }
+}
